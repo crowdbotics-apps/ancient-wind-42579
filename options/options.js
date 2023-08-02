792421508dd4e085b1a2a76dfa86b51ad2cfdda5
Crowdbotics <team@crowdbotics.com>
2023-08-02 01:37:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ancient-wind-42579",
+  url: "https://ancient-wind-42579.botics.co",
+  api: "https://ancient-wind-42579.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
